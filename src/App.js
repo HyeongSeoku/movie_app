f34556809b87a9f0c7206f71531d1ce4025ef8e3
HyeongSeoku <gudtjr3437@naver.com>
2020-02-08 15:40:04 +0900

    chapter 4-2

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
 import React from 'react';
 import axios from 'axios';
-
+import Movie from './Movie';
 
 
 class App extends React.Component{
-  //class react component dont have return (cause it isn't function)
-  //"render" have.
+
   state = {
    isLoading: true,
    movies:[]
   };
   getMovies= async () =>{
-    const movies = await axios.get("https://yts.mx/api/v2/list_movies.json://yts-proxy.now.sh/list_movies.json");
-    console.log(movies);
+    const {data: {data:{movies}}} = await axios.get("https://yts.mx/api/v2/list_movies.json://yts-proxy.now.sh/list_movies.json");
+    this.setState({movies, isLoading:false});
   }
   componentDidMount(){
    this.getMovies();
   }
   render(){
-    const{isLoading} =this.state;
-    return <div>{isLoading ? "Loading..":"We are ready"}</div>;
-  }
-  
+    const{isLoading, movies} =this.state;
+    return <div>{isLoading ? "Loading..":movies.map(movie => (
+      <Movie 
+      key={movie.id}
+      id={movie.id} 
+      year={movie.year}
+      title={movie.title} 
+      summary={movie.summary} 
+      poster={movie.medium_cover_image}
+      />
+    ))}
+    </div>;
+  } 
 }
 export default App;
  
